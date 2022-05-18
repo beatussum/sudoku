9cd8c5def742d9252c4c64ed3890ecc8a7a3d1ae
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2022-05-18 20:58:48 +0200

    add packaging
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

--- src/sudoku/gui/__main__.py
+++ src/sudoku/gui/__main__.py
@@ -3,9 +3,12 @@ from sudoku.gui import MainWindow
 from PyQt5 import QtWidgets
 import sys
 
-app = QtWidgets.QApplication(sys.argv)
+def main(argv: list[str]) -> int:
+    app = QtWidgets.QApplication(argv)
 
-main = MainWindow()
-main.show()
+    main = MainWindow()
+    main.show()
 
-app.exec()
+    return app.exec()
+
+sys.exit(main(sys.argv))
